6e832082485a6a53afc437f5833218ab559bffe3
Danylo Sementsov <daniilsemencov01@gmail.com>
2024-06-26 15:02:01 +0200

    Initial i guess, i dont know what it means anyway.

---
+++ src/main/java/net/flo/stargaze/StargazeClient.java
@@ -0,0 +1,10 @@
+package net.flo.stargaze;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StargazeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
