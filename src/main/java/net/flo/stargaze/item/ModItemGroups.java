f597c068e9e9f3022e195d6887cd28e9f0d28682
Danylo Sementsov <daniilsemencov01@gmail.com>
2024-06-27 21:09:11 +0200

    New update, still going through with the tutorial

---
+++ src/main/java/net/flo/stargaze/item/ModItemGroups.java
@@ -0,0 +1,28 @@
+package net.flo.stargaze.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.flo.stargaze.Stargaze;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup STARGAZE_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(Stargaze.MOD_ID, "clearancejade"), FabricItemGroup.builder().displayName(Text.translatable("itemgroup.clearancejade"))
+                    .icon(() -> new ItemStack(ModItems.CLEARANCEJADE)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.CLEARANCEJADE);
+                        entries.add(ModItems.POLISHEDJADE);
+
+
+
+                    }).build());
+    public static void registerItemGroups(){
+        Stargaze.LOGGER.info("Registering Item Groups for" + Stargaze.MOD_ID);
+
+
+    }
+
+}
